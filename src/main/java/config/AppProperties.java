1a447a91ede2d8e606ccb0970a2d2ee7fcf3e0ca
yeti98 <yeti98@github.com>
2019-11-04 16:30:05 +0700

    Add config

--- src/main/java/config/AppProperties.java
+++ src/main/java/config/AppProperties.java
@@ -2,8 +2,8 @@ package config;
 
 public class AppProperties {
 //    public static final String HOST = "192.168.0.103";
-    public static final String HOST = "localhost";
+    public static final String HOST = "192.168.43.26";
     public static final int PORT = 5000;
     public static final int REGISTRY_PORT = 5001;
-    public static final String REGISTRY_NAME = "raven";
+    public static final String REGISTRY_NAME = "name";
 }
